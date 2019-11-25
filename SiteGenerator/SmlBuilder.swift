4b89f3f28e4211372412721d6a48c8aa7a070a04
Christian Noll <christiannoll@gmx.net>
2019-11-25 20:54:13 +0100

    Added class SmlNode

--- SiteGenerator/SmlBuilder.swift
+++ SiteGenerator/SmlBuilder.swift
@@ -42,4 +42,6 @@ class SmlBuilder {
         }
         return s
     }
+    
+    
 }
---
+++ SiteGenerator/SmlNode.swift
@@ -0,0 +1,78 @@
+import Foundation
+
+struct SmlAttribute {
+    let key: String
+    let value: String
+    
+    init(_ key: String, _ value: String) {
+        self.key = key
+        self.value = value
+    }
+}
+
+struct SmlAttributeKey<A> {
+    let key: String
+    init(_ key: String) { self.key = key }
+}
+
+struct SmlElement {
+    let name: String
+    let attribs: [SmlAttribute]
+    let children: [SmlNode]?
+    
+    init(_ name: String, _ attribs: [SmlAttribute], _ children: [SmlNode]?) {
+        self.name = name
+        self.attribs = attribs
+        self.children = children
+    }
+}
+
+enum SmlNode {
+    case element(SmlElement)
+    case text(String)
+}
+
+extension SmlNode: ExpressibleByStringLiteral {
+    init(stringLiteral value: String) {
+        self = .text(value)
+    }
+}
+
+func node(_ name: String, _ attribs: [SmlAttribute], _ children: [SmlNode]?) -> SmlNode {
+    return .element(.init(name, attribs, children))
+}
+
+func node(_ name: String, _ children: [SmlNode]?) -> SmlNode {
+    return node(name, [], children)
+}
+
+func p(_ attribs: [SmlAttribute], _ children: [SmlNode]) -> SmlNode {
+    return node("p", attribs, children)
+}
+
+func p(_ children: [SmlNode]) -> SmlNode {
+    return p([], children)
+}
+
+func a(_ attribs: [SmlAttribute], _ children: [SmlNode]) -> SmlNode {
+    return node("a", attribs, children)
+}
+
+func a(_ children: [SmlNode]) -> SmlNode {
+    return a([], children)
+}
+
+infix operator =>
+func => <A> (key: SmlAttributeKey<A>, value: A) -> SmlAttribute {
+    return .init(key.key, "\(value)")
+}
+
+let href = SmlAttributeKey<String>("href")
+
+let document: SmlNode =
+    p([
+        "Welcome to you, who has come here. See ",
+        a([href => "/more"], ["more"]),
+        "."
+        ])
+
