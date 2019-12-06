f32cb7c3b5d2eb7d783bdacec065c1cf5533430e
Christian Noll <christiannoll@gmx.net>
2019-12-06 21:05:05 +0100

    Added file 'TagItem'

---
+++ SiteGenerator/TagItem.swift
@@ -0,0 +1,42 @@
+class TagItem {
+    
+    private let _key: String
+    private var posts: [Item] = []
+    
+    var key: String {
+        get { return _key }
+    }
+    
+    init(_ key: String) {
+        self._key = key
+    }
+    
+    public func addPost(_ post: Item) {
+        posts.append(post)
+    }
+    
+    public func renderTagItem() -> SmlNode {
+        var liChildren: [SmlNode] = []
+        let link = a([href => createLinkUrl()], [createLinkTitle()])
+        liChildren.append(link)
+        let l = li(liChildren)
+        return l
+    }
+    
+    public func renderTagItemPosts() -> [SmlNode] {
+        var tagItemPosts: [SmlNode] = [newLine]
+        
+        for post: Item in posts {
+            tagItemPosts.append(post.renderPost())
+        }
+        return tagItemPosts
+    }
+    
+    private func createLinkUrl() -> String {
+        return Page.baseUrl + key + "/"
+    }
+    
+    private func createLinkTitle() -> SmlNode {
+        return .text(key + " (" + String(posts.count) + ")")
+    }
+}
