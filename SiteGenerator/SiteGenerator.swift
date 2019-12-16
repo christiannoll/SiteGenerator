47017f80668295d29be35f3012a2c77f54f38080
Christian Noll <christiannoll@gmx.net>
2019-12-16 09:48:22 +0100

    Added new file 'SiteGenerator.swift'

---
+++ SiteGenerator/SiteGenerator.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+struct SiteGenerator {
+    
+    func generate() {        
+        let contentParser = ContentParser()
+        let posts = contentParser.parse()
+        
+        let writer = PageWriter()
+        
+        let homePage = HomePage(posts)
+        writer.writeHomePage(homePage.render())
+        
+        for post: Item in posts {
+            let page = PostPage(post)
+            writer.writePostPage(post, page.render())
+        }
+        
+        let archiveFactory = ArchiveFactory()
+        let archive = archiveFactory.createArchive(posts)
+        let archivePage = ArchivePage(archive);
+        writer.writeArchivePage(archivePage.render())
+        
+        let archiveYears = archive.years
+        for year: ArchiveYear in archiveYears {
+            for month: ArchiveMonth in year.months {
+                let page = ArchiveMonthPage(month)
+                writer.writeArchiveMonthPage(month, page.render())
+            }
+        }
+        
+        let indexFactory = IndexFactory()
+        let index = indexFactory.createIndex(posts)
+        let indexPage = IndexPage(index)
+        writer.writeIndexPage(indexPage.render())
+        
+        let tagsFactory = TagsFactory()
+        let tags = tagsFactory.createTags(posts)
+        let tagsPage = TagsPage(tags)
+        writer.writeTagsPage(tagsPage.render())
+        
+        let tagItems = tags.tagItems
+        for tagItem: TagItem in tagItems {
+            let page = TagItemPage(tagItem)
+            writer.writeTagItemPage(tagItem, page.render())
+        }
+        
+        let impressumPage = ImpressumPage()
+        writer.writeImpressumPage(impressumPage.render())
+    }
+}
--- SiteGenerator/main.swift
+++ SiteGenerator/main.swift
@@ -1,49 +1,7 @@
 import Foundation
 
-let contentParser = ContentParser()
-let posts = contentParser.parse()
-
-let writer = PageWriter()
-
-let homePage = HomePage(posts)
-writer.writeHomePage(homePage.render())
-
-for post: Item in posts {
-    let page = PostPage(post)
-    writer.writePostPage(post, page.render())
-}
-
-let archiveFactory = ArchiveFactory()
-let archive = archiveFactory.createArchive(posts)
-let archivePage = ArchivePage(archive);
-writer.writeArchivePage(archivePage.render())
-
-let archiveYears = archive.years
-for year: ArchiveYear in archiveYears {
-    for month: ArchiveMonth in year.months {
-        let page = ArchiveMonthPage(month)
-        writer.writeArchiveMonthPage(month, page.render())
-    }
-}
-
-let indexFactory = IndexFactory()
-let index = indexFactory.createIndex(posts)
-let indexPage = IndexPage(index)
-writer.writeIndexPage(indexPage.render())
-
-let tagsFactory = TagsFactory()
-let tags = tagsFactory.createTags(posts)
-let tagsPage = TagsPage(tags)
-writer.writeTagsPage(tagsPage.render())
-
-let tagItems = tags.tagItems
-for tagItem: TagItem in tagItems {
-    let page = TagItemPage(tagItem)
-    writer.writeTagItemPage(tagItem, page.render())
-}
-
-let impressumPage = ImpressumPage()
-writer.writeImpressumPage(impressumPage.render())
+let generator = SiteGenerator()
+generator.generate()
 
 /*let s = "text1 [title](url) text2"
 let elements = MarkdownParser.parse(text: s)
