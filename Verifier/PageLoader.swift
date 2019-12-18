5cc6d5fc5b0520babfee4a87ff77e0f460cff12b
Christian Noll <christiannoll@gmx.net>
2019-12-18 21:11:23 +0100

    Refactoring Verifier classes

--- Verifier/PageLoader.swift
+++ Verifier/PageLoader.swift
@@ -2,14 +2,14 @@ import Foundation
 
 class PageLoader {
     
-    func loadPage(from urlString: String, completionHandler: @escaping (_ URLResponse: URLResponse?) ->()) {
+    func loadPage(from urlString: String, completionHandler: @escaping (_ URLResponse: URLResponse?, _ urlString: String) ->()) {
         let session = URLSession.shared
         let url = URL(string: urlString)
         
         let sema = DispatchSemaphore(value: 0)
         
         let task = session.dataTask(with: url!) { (data, response, error) in
-            completionHandler(response)
+            completionHandler(response, urlString)
             
             sema.signal()
         }
--- Verifier/PageVerifier.swift
+++ Verifier/PageVerifier.swift
@@ -2,18 +2,41 @@ import Foundation
 
 class PageVerifier {
     
+    private let posts: [Item]
+    
+    init() {
+        let contentParser = ContentParser()
+        posts = contentParser.parse()
+    }
+    
     func verify() {
-        //let contentParser = ContentParser()
-        // posts = contentParser.parse()
-        
+        loadHomePage()
+        loadPostPages()
+    }
+    
+    private func loadHomePage() {
+        loadPage(relPath: "/index.html")
+    }
+    
+    private func loadPostPages() {
+        for post in posts {
+            var relPath = ""
+            relPath.append(PostBuilder.createDatePath(post))
+            relPath.append(post.name)
+            relPath.append("/index.html")
+            loadPage(relPath: relPath)
+        }
+    }
+    
+    private func loadPage(relPath: String) {
         let pageLoader = PageLoader()
-        pageLoader.loadPage(from: Page.baseUrl + "/index.html") { (response) in
+        pageLoader.loadPage(from: Page.baseUrl + relPath) { (response, urlString) in
             
             guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
-                print("Server error!")
+                print("Error- loading: " + urlString)
                 return
             }
-            print("ok")
+            print("OK- loading: " + urlString)
         }
     }
 }
