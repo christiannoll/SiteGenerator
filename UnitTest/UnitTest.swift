459b989289f24e8bf328879b3bac53283b811b74
Christian Noll <christiannoll@gmx.net>
2019-12-19 23:53:01 +0100

    Added membership of testing files to UnitTest

---
+++ UnitTest/UnitTest.swift
@@ -0,0 +1,31 @@
+import XCTest
+
+class UnitTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        let s = "text1 [title](url) text2"
+        let smlBuilder = SmlBuilder()
+        let text = smlBuilder.render(s)
+        
+        XCTAssert(text == "text1<a href=\"url\">title</a> text2")
+        
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
