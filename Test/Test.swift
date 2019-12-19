5bba9c5ddd037876e82b6f8ff70dbd4006562943
Christian Noll <christiannoll@gmx.net>
2019-12-19 18:40:05 +0100

    Added test target

---
+++ Test/Test.swift
@@ -0,0 +1,30 @@
+import XCTest
+import SiteGenerator
+
+class Test: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        let s = "text1 [title](url) text2"
+        let elements = MarkdownParser.parse(text: s)
+ 
+        
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
