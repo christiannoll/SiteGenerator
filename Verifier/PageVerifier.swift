0fd0aa7eb7fe6f47aedf16d1ad88f9760375e679
Christian Noll <christiannoll@gmx.net>
2020-01-10 21:19:51 +0100

    Added Verifier Result

--- Verifier/PageVerifier.swift
+++ Verifier/PageVerifier.swift
@@ -9,39 +9,47 @@ class PageVerifier {
     }
     
     func verify() {
-        loadHomePage()
-        loadPostPages()
-        loadPostLinks()
-        loadArchivePage()
-        loadArchiveMonthPages()
-        loadIndexPage()
-        loadIndexItemPages()
-        loadTagsPage()
-        loadTagItemPages()
-        loadImpressumPage()
+        var res = true
+        res = res && loadHomePage()
+        res = res && loadPostPages()
+        res = res && loadPostLinks()
+        res = res && loadArchivePage()
+        res = res && loadArchiveMonthPages()
+        res = res && loadIndexPage()
+        res = res && loadIndexItemPages()
+        res = res && loadTagsPage()
+        res = res && loadTagItemPages()
+        res = res && loadImpressumPage()
+        print("\nVERIFIER RESULT: \(res ? "OK" : "ERROR")")
     }
     
-    private func loadHomePage() {
-        loadPage(relPath: "index.html")
+    private func loadHomePage() -> Bool {
+        return loadPage(relPath: "index.html")
     }
     
-    private func loadPostPages() {
+    private func loadPostPages() -> Bool {
+        var res = true
         for post in posts {
             var relPath = ""
             relPath.append(PostBuilder.createDatePath(post))
             relPath.append(post.name)
             relPath.append("/index.html")
-            loadPage(relPath: relPath)
+            res = res && loadPage(relPath: relPath)
         }
+        return res
     }
     
-    private func loadPostLinks() {
+    private func loadPostLinks() -> Bool {
+        var res = true
         for post in posts {
             let markdownNodes = MarkdownParser.parse(text: post.data)
             var links: [String: String] = [:]
             parse(markdownNodes, &links)
-            links.forEach { loadUrl(($0,$1)) }
+            links.forEach {
+                res = res && loadUrl(($0,$1))
+            }
         }
+        return res
     }
     
     private func parse(_ markdownNodes: [MarkdownNode], _ links: inout [String: String]) {
@@ -90,11 +98,12 @@ class PageVerifier {
         }
     }
     
-    private func loadArchivePage() {
-        loadPage(relPath: "archive/index.html")
+    private func loadArchivePage() -> Bool {
+        return loadPage(relPath: "archive/index.html")
     }
     
-    private func loadArchiveMonthPages() {
+    private func loadArchiveMonthPages() -> Bool {
+        var res = true
         let archiveFactory = ArchiveFactory()
         let archive = archiveFactory.createArchive(posts)
         
@@ -108,66 +117,77 @@ class PageVerifier {
                 }
                 relPath.append(monthStr)
                 relPath.append("/index.html")
-                loadPage(relPath: relPath)
+                res = res && loadPage(relPath: relPath)
             }
         }
+        return res
     }
     
-    private func loadIndexPage() {
-        loadPage(relPath: "index/index.html")
+    private func loadIndexPage() -> Bool {
+        return loadPage(relPath: "index/index.html")
     }
     
-    private func loadIndexItemPages() {
+    private func loadIndexItemPages() -> Bool {
+        var res = true
         let indexFactory = IndexFactory()
         let index = indexFactory.createIndex(posts)
         
         let indexItems = index.indexItems
         for indexItem in indexItems {
             let relPath = "index/" + indexItem.key.convertToUrlPath() + "/index.html"
-            loadPage(relPath: relPath)
+            res = res && loadPage(relPath: relPath)
         }
+        return res
     }
     
-    private func loadTagsPage() {
-        loadPage(relPath: "tags/index.html")
+    private func loadTagsPage() -> Bool {
+        return loadPage(relPath: "tags/index.html")
     }
     
-    private func loadTagItemPages() {
+    private func loadTagItemPages() -> Bool {
+        var res = true
         let tagsFactory = TagsFactory()
         let tags = tagsFactory.createTags(posts)
         
         let tagItems = tags.tagItems
         for tagItem in tagItems {
             let relPath = "tags/" + tagItem.key.convertToUrlPath() + "/index.html"
-            loadPage(relPath: relPath)
+            res = res && loadPage(relPath: relPath)
         }
+        return res
     }
     
-    private func loadImpressumPage() {
-        loadPage(relPath: "impressum/index.html")
+    private func loadImpressumPage() -> Bool {
+        return loadPage(relPath: "impressum/index.html")
     }
     
-    private func loadPage(relPath: String) {
+    private func loadPage(relPath: String) -> Bool {
+        var res = false
         let pageLoader = PageLoader()
+        
         pageLoader.loadPage(from: Page.baseUrl + relPath) { (response, urlString) in
-            
             guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
                 print("Error- loading page: " + urlString)
                 return
             }
             print("OK- loading page: " + urlString)
+            res = true
         }
+        return res
     }
     
-    private func loadUrl(_ link: (String,String)) {
+    private func loadUrl(_ link: (String,String)) -> Bool {
+        var res = false
         let pageLoader = PageLoader()
+        
         pageLoader.loadPage(from: link.0) { (response, urlString) in
-            
             guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
                 print("Error- loading \(link.1): " + urlString)
                 return
             }
             print("OK- loading \(link.1): " + urlString)
+            res = true
         }
+        return res
     }
 }
