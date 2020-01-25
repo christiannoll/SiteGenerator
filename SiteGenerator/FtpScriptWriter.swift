9c713e97c7f42830a54a18238c010793c314c993
Christian Noll <christiannoll@gmx.net>
2020-01-25 16:44:32 +0100

    Changed 'FtpScriptWriter'

--- SiteGenerator/FtpScriptWriter.swift
+++ SiteGenerator/FtpScriptWriter.swift
@@ -2,18 +2,52 @@ import Foundation
 
 class FtpScriptWriter {
     
+    private let baseUrl = URL(fileURLWithPath: PageWriter.baseDir)
     private let folderNames = ["2019", "2020", "archive", "images", "impressum", "index", "statistic", "tags"]
     
-    func writeScript() {
-        var content = ""
-        for folderName in folderNames {
-            let items = getFolderContent(folderName)
-            items.forEach { content.append($0 + "\n") }
-        }
-        writeScriptFile(content)
+    private var script = ""
+    
+    init() {
+        generateDeleteCommands()
     }
     
-    private func getFolderContent(_ folderName: String) -> [String] {
+    func writeScript() {
+        generateMkdirCommands()
+        generatePutCommands()
+        writeScriptFile(script)
+    }
+    
+    private func generateDeleteCommands() {
+        for folderName in folderNames {
+            let files = getFolderContent(folderName, false)
+            files.forEach { script.append("rm " + $0 + "\n") }
+        }
+        
+        for folderName in folderNames {
+            var dirs = getFolderContent(folderName, true)
+            dirs.reverse()
+            dirs.append(folderName)
+            dirs.forEach { script.append("rm " + $0 + "\n") }
+        }
+    }
+    
+    private func generateMkdirCommands() {
+        for folderName in folderNames {
+            var dirs = getFolderContent(folderName, true)
+            dirs.insert(folderName, at: 0)
+            dirs.forEach { script.append("mkdir " + $0 + "\n") }
+        }
+    }
+    
+    private func generatePutCommands() {
+        for folderName in folderNames {
+            let files = getFolderContent(folderName, false)
+            files.forEach { script.append("put " + $0 + " " + $0 + "\n") }
+        }
+        script.append("bye")
+    }
+    
+    private func getFolderContent(_ folderName: String, _ onlyFolder: Bool) -> [String] {
         let fm = FileManager.default
         let path = PageWriter.baseDir + folderName
         
@@ -22,11 +56,14 @@ class FtpScriptWriter {
         let resourceKeys = Set<URLResourceKey>([.nameKey, .isDirectoryKey])
         let directoryURL = URL(string: path)
         let directoryEnumerator = fm.enumerator(at: directoryURL!, includingPropertiesForKeys: Array(resourceKeys), options: .skipsHiddenFiles)!
-        let baseUrl = URL(fileURLWithPath: PageWriter.baseDir)
+        
         while case let fileUrl as URL = directoryEnumerator.nextObject() {
-            //if (!fileUrl.hasDirectoryPath) {
+            if onlyFolder && fileUrl.hasDirectoryPath {
                 items.append(fileUrl.relativePath(from: baseUrl)!)
-            //}
+            }
+            else if !onlyFolder && !fileUrl.hasDirectoryPath {
+                items.append(fileUrl.relativePath(from: baseUrl)!)
+            }
         }
                 
         return items
