20bb24297dabfa6e0c1efb45ef0dd0e798094640
Christian Noll <christiannoll@gmx.net>
2020-04-24 20:03:05 +0200

    Added new class 'SearchIndexBuilder'

---
+++ SiteGenerator/SearchIndexBuilder.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+class SearchIndexBuilder {
+    
+    /*func split(_ text: String) {
+        let words = text.components(separatedBy: .whitespaces)
+        for word in words {
+            let trimmedWord = word.trimmingCharacters(in: .punctuationCharacters)
+        }
+    }*/
+}
--- Verifier/PageVerifier.swift
+++ Verifier/PageVerifier.swift
@@ -46,74 +46,16 @@ class PageVerifier {
     
     private func loadPostLinks() -> Bool {
         var res = true
+        let linkParser = LinkParser()
         for post in posts {
             let markdownNodes = MarkdownParser.parse(text: post.data)
             var links: [String: String] = [:]
-            parse(markdownNodes, &links)
+            linkParser.parse(markdownNodes, &links)
             links.forEach {
                 res = res && loadUrl((SmlBuilder.buildUrl($0), $1))
             }
         }
         return res
-    }
-    
-    private func parse(_ markdownNodes: [MarkdownNode], _ links: inout [String: String]) {
-        for markDownNode in markdownNodes {
-            switch markDownNode {
-            case .bold(let nodes):
-                parse(nodes, &links)
-            case .italic(let nodes):
-                parse(nodes, &links)
-            case .code(let nodes):
-                parse(nodes, &links)
-            case .parenthesis(let nodes):
-                parse(nodes, &links)
-            case .brackets(let nodes):
-                parse(nodes, &links)
-            case .olistelement(let nodes):
-                parse(nodes, &links)
-            case .ulistelement(let nodes):
-                parse(nodes, &links)
-            case .link(let nodes):
-                var strings: [String] = []
-                parseLink(nodes, &strings)
-                if strings.count == 2 {
-                    links[strings[0]] = strings[1]
-                }
-            case .ulist(let nodes):
-                parse(nodes, &links)
-            case .olist(let nodes):
-                parse(nodes, &links)
-            default:
-                break
-            }
-        }
-    }
-    
-    private func parseLink(_ markdownNodes: [MarkdownNode], _ strings: inout [String]) {
-        for markDownNode in markdownNodes {
-            switch markDownNode {
-            case .parenthesis(let nodes):
-                strings.append(parseLinkText(nodes))
-            case .brackets(let nodes):
-                strings.append(parseLinkText(nodes))
-            default:
-                break
-            }
-        }
-    }
-    
-    private func parseLinkText(_ markdownNodes: [MarkdownNode]) -> String {
-        var s = ""
-        for markDownNode in markdownNodes {
-            switch markDownNode {
-            case .text(let text):
-                s.append(text)
-            default:
-                break
-            }
-        }
-        return s
     }
     
     private func loadArchivePage() -> Bool {
