8d2556b1a084675c80c62e122d16fd23e9b272f3
Christian Noll <christiannoll@gmx.net>
2020-04-24 21:58:41 +0200

    Added function buildIndex

--- SiteGenerator/SearchIndexBuilder.swift
+++ SiteGenerator/SearchIndexBuilder.swift
@@ -2,10 +2,26 @@ import Foundation
 
 class SearchIndexBuilder {
     
-    /*func split(_ text: String) {
+    func buildIndex(_ posts: [Item]) {
+        var words: [String] = []
+        for post in posts {
+            words.append(contentsOf: split(post.title))
+            for index in post.indices {
+                words.append(contentsOf: split(index))
+            }
+            for link in post.links {
+                words.append(contentsOf: split(link.1))
+            }
+        }
+    }
+    
+    private func split(_ text: String) -> [String] {
+        var trimmedWords: [String] = []
         let words = text.components(separatedBy: .whitespaces)
         for word in words {
             let trimmedWord = word.trimmingCharacters(in: .punctuationCharacters)
+            trimmedWords.append(trimmedWord)
         }
-    }*/
+        return trimmedWords
+    }
 }
