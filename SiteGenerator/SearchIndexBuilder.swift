c72fd5fb4ff8db2e6c8619bcca61e78b31439d79
Christian Noll <christiannoll@gmx.net>
2020-04-26 18:01:58 +0200

    Updated function buildIndex

--- SiteGenerator/SearchIndexBuilder.swift
+++ SiteGenerator/SearchIndexBuilder.swift
@@ -3,21 +3,29 @@ import Foundation
 struct SearchIndexBuilder {
     
     func buildIndex(_ posts: [Item]) {
+        var searchIndex = [String: Set<Int>]()
         var words: [String] = []
+        
         for post in posts {
             words.append(contentsOf: split(post.title))
             words.append(contentsOf: post.indices.map { split($0) }.joined())
             words.append(contentsOf: post.tags.map { split($0) }.joined())
             words.append(contentsOf: post.links.map { split($0.1) }.joined())
-        }
-        
-        for word in words {
-            let upperBound = word.count-2
-            for index in 0..<upperBound {
-                let start = word.index(word.startIndex, offsetBy: index)
-                let end = word.index(start, offsetBy: +3)
-                let range = start..<end
-                let part = word[range]
+            
+            for word in words {
+                let upperBound = word.count-2
+                for index in 0..<upperBound {
+                    let start = word.index(word.startIndex, offsetBy: index)
+                    let end = word.index(start, offsetBy: +3)
+                    let range = start..<end
+                    let part = String(word[range])
+                    if var postIndices = searchIndex[part] {
+                        postIndices.insert(post.id)
+                    }
+                    else {
+                        searchIndex[part] = Set<Int>([post.id])
+                    }
+                }
             }
         }
     }
