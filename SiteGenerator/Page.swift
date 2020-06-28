eac80b86f1f152ce2032d66c842a828245079bfc
Christian Noll <christiannoll@gmx.net>
2020-06-28 13:03:47 +0200

    Undo unwanted changes

--- SiteGenerator/Page.swift
+++ SiteGenerator/Page.swift
@@ -2,8 +2,8 @@ import Foundation
 
 class Page {
     
-    //public static let baseUrl = "http://localhost:8000/"
-    public static let baseUrl = "https://www.vnzn.de/"
+    public static let baseUrl = "http://localhost:8000/"
+    //public static let baseUrl = "https://www.vnzn.de/"
     
     static let homepageTitle = "v.n.z.n"
     static let homepageTagline = "Christian Noll"
