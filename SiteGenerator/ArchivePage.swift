f75b6e37e9b1b4ebc13281ab580f2026396a9b29
Christian Noll <christiannoll@gmx.net>
2026-01-31 19:01:41 +0100

    Add max-link that shows all posts

--- SiteGenerator/ArchivePage.swift
+++ SiteGenerator/ArchivePage.swift
@@ -10,9 +10,17 @@ class ArchivePage : Page {
     
     override func renderContent() -> SmlNode {
         var mainChildren: [SmlNode] = [newLine]
+        mainChildren.append(renderMaxLink())
+        mainChildren.append(newLine)
         mainChildren.append(archive.renderArchiveIndex())
         
         mainChildren.append(newLine)
         return main(mainChildren)
+    }
+
+    func renderMaxLink() -> SmlNode {
+        let link = a([href => (Page.baseUrl + "max/")], [SiteGeneratorEnv.forGerman() ? "Ein langer Strom": "A Long Stream"])
+        let para = p([link])
+        return para
     }
 }
